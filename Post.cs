2118b81c91304c1b9eb67e95f10f8166347d03fe
nishi0000 <sigotoyou.mail00@gmail.com>
2026-07-09 21:09:01 +0900

    second commit

---
+++ Post.cs
@@ -0,0 +1,24 @@
+﻿namespace BoardApp;
+
+class Post
+{
+    int Id {get;set;}
+    string? Author {get;set;}
+
+    string? Message {get;set;}
+
+    DateTime CreatedAt {get;set;}
+
+    List<Post> post = [{Id=1,"test","test",DateTime.Now}];
+
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    void ShowPosts()
+    {
+        
+    }
+
+}
