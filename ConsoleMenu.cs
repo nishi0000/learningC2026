a5fc13540b0d3fa0a486bd0468eee85f9400ca00
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2026-07-15 11:00:59 +0900

    Add files via upload

---
+++ ConsoleMenu.cs
@@ -0,0 +1,106 @@
+﻿
+namespace BoardApp;
+
+class ConsoleMenu
+{
+    PostRepository _postRepository;
+
+    public ConsoleMenu(PostRepository postRepository){
+
+        _postRepository = postRepository;
+        
+    }
+
+    public void ShowMenu()
+    {
+        List<Post> posts = _postRepository.GetAll();
+
+        do 
+        {
+            int choice = AskChoice();
+            if (choice == 1)
+            {
+                if (posts.Count() <= 0)
+                {
+                    
+                    Console.WriteLine($"一覧なし！"); 
+                }
+                else
+                {
+                    ShowPosts(posts);
+                }
+            }
+            else if(choice == 2)
+            {
+                string name = InputMessage("名前");
+                string message = InputMessage("メッセージ");
+
+                _postRepository.Add(name,message);
+                ShowPosts(posts);
+
+            }
+            else
+            {
+                break;
+            }
+
+        }while(true);
+    }
+    private int AskChoice()
+    {
+        int userChoice;
+
+        while (true)
+        {
+            Console.WriteLine("数値を入力してください（1: 一覧 / 2: 投稿 / 0: 終了）");
+            var input = Console.ReadLine();
+
+            if(int.TryParse(input, out int choice))
+            {
+                if( choice < 0 || 2 < choice)
+                {
+                    Console.WriteLine("0〜2 を入力してください");
+                }
+                else
+                {
+                    userChoice = choice;
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("数値を入力してください");
+            }
+        }
+
+        return userChoice;
+    }
+
+    private void ShowPosts(List<Post> posts)
+    {
+        foreach(Post element in posts){
+            Console.WriteLine($"[{element.Id}] {element.CreatedAt.ToString("yyyy/MM/dd HH:mm")} {element.Author}: {element.Message}"); 
+        }
+        
+    }
+    private string InputMessage(string item)
+    {
+        do
+        {
+            Console.WriteLine($"{item}を入力してください。");
+            string? input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine($"入力は必須です。");
+            }
+            else
+            {
+                return input;
+            }
+            
+        }while(true);
+        
+    }
+
+}
---
+++ PostRepository.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json;
+
+namespace BoardApp;
+
+class PostRepository
+{
+    private List<Post> posts = [];
+    string path = @"posts.json";
+
+    public PostRepository()
+    {
+        posts = ReadJson();
+    }
+
+    private List<Post> ReadJson()
+    {
+        if(File.Exists(path))
+        {
+            var load = JsonSerializer.Deserialize<List<Post>>(File.ReadAllText(path));
+            return load ?? [];
+        }
+
+        return [];
+    }
+
+    public List<Post> GetAll()
+    {
+        return posts;
+    }
+
+    public void Add(string name,string message)
+    {
+        int id;
+        if(posts.Count == 0)
+        {
+            id = 1;
+        }
+        else
+        {
+            id = posts.Select(x => x.Id).Max() + 1;
+        }
+
+        posts.Add(new Post(name,message) {Id=id,CreatedAt=DateTime.Now});
+        WriteJson();
+
+    }
+
+    private void WriteJson()
+    {
+        // インデント（整形）を有効にする
+        var options = new JsonSerializerOptions 
+        { 
+            WriteIndented = true 
+        };
+
+        File.WriteAllText(path,JsonSerializer.Serialize(this.posts,options));
+
+    }
+
+}
